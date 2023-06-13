1b19036f0b7c524330770cd26931151640f7a4a1
CHIRAG DUA <68508893+chiragdua26@users.noreply.github.com>
2023-06-13 15:08:14 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,13 @@
 
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { ...yourOtherReducers } from './reducers';
+
+const rootReducer = combineReducers({
+  form: formReducer,
+  ...yourOtherReducers,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
