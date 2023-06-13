3d9eaf40534a8d95918f4c2961da17a618cfec99
CHIRAG DUA <68508893+chiragdua26@users.noreply.github.com>
2023-06-13 15:03:44 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResumeForm from './ResumeForm';
+
+const App = () => {
+  return (
+    <div className="container">
+      <ResumeForm />
+    </div>
+  );
+};
+
+export default App;
